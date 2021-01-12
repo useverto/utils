e17f8dd0f61ba67b686f4e9b84fcecf07e3f0d03
John Letey <johnletey@gmail.com>
2021-01-12 23:07:40 +0000

    feat: lock tokens

--- src/lock.ts
+++ src/lock.ts
@@ -1,26 +1,48 @@
-import Community from "community-js";
-import arweave from "./utils/client";
-
-import { contractID } from "./balance";
+import Arweave from "arweave";
 import { readFileSync } from "fs";
 import { join } from "path";
+import { interactWrite } from "smartweave";
 
-const community: Community = new Community(arweave);
-const key = JSON.parse(
+const client = new Arweave({
+  host: "arweave.net",
+  port: 443,
+  protocol: "https",
+});
+
+const jwk = JSON.parse(
   new TextDecoder().decode(
     readFileSync(join(__dirname, "../assets/keyfile.json"))
   )
 );
 
-async function initialize() {
-  await community.setCommunityTx(contractID);
-  await community.setWallet(key);
+const numberPerUnlock = 937500;
+const lengths = [
+  131400, // 6 months
+  262800, // 12 months
+  394200, // 18 months
+  525600, // 24 months
+  657000, // 30 months
+  788400, // 36 months
+  919800, // 42 months
+  1051200, // 48 months
+];
+
+async function lock() {
+  for (const length of lengths) {
+    const txID = await interactWrite(
+      client,
+      jwk,
+      "usjm4PCxUd5mtaon7zc97-dt-3qf67yPyqgzLnLqk5A",
+      {
+        function: "lock",
+        qty: numberPerUnlock,
+        lockLength: length,
+      }
+    );
+    console.log(
+      `Locked ${numberPerUnlock} VRT tokens for ${length} blocks.\n\ttxID = ${txID}`
+    );
+  }
 }
 
-async function vote() {
-  await initialize();
-  let lockTX = await community.lockBalance(25000000, 129600);
-  console.log(lockTX);
-}
-
-vote();
+lock();
