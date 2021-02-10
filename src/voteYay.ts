20ad68f868403ac98b0681069eba9f591192fed4
John Letey <johnletey@gmail.com>
2021-02-10 10:07:56 +0000

    chore: update voteYay function

--- src/voteYay.ts
+++ src/voteYay.ts
@@ -16,19 +16,17 @@ const jwk = JSON.parse(
 );
 
 async function voteYay() {
-  for (let vote = 53; vote <= 55; vote++) {
-    const txID = await interactWrite(
-      client,
-      jwk,
-      "usjm4PCxUd5mtaon7zc97-dt-3qf67yPyqgzLnLqk5A",
-      {
-        function: "vote",
-        id: vote,
-        cast: "yay",
-      }
-    );
-    console.log(`Vote ${vote}: ${txID}`);
-  }
+  const txID = await interactWrite(
+    client,
+    jwk,
+    "usjm4PCxUd5mtaon7zc97-dt-3qf67yPyqgzLnLqk5A",
+    {
+      function: "vote",
+      id: 58,
+      cast: "yay",
+    }
+  );
+  console.log(`Vote ${58}: ${txID}`);
 }
 
 voteYay();
