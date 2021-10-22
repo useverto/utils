81f476694915171af95631d555319aeffdce601f
Tate Berenbaum <tateflyer1@gmail.com>
2021-10-21 23:38:28 -0400

    fix: update script to use new transferLocked function

--- src/mintLocked.ts
+++ src/mintLocked.ts
@@ -19,34 +19,121 @@ const votes: {
   recipient: string;
   qty: number;
   lockLength: number;
-  note: string;
 }[] = [
-  // Mar 29th, 2021 - 654719
-  // May 03rd, 2021 - 678484
-  // Difference     = 23765
-
   // 12 months      ~ 262800
   // 18 months      ~ 394200
   // 24 months      ~ 525600
-
   {
-    recipient: "VlpcjrQ8mirGC-gsnkYbSscYY-sFkLxkRhjPrbdDfyU",
-    qty: 725694,
-    lockLength: 239035,
-    note: "(#2) Strategic Partner 12-Month Mint Lock",
+    recipient: "NDQwbjnrUIWXyOVyc3k_vn6dzF8ZuqZTy8vQbz5vjSY",
+    qty: 1041667,
+    lockLength: 262800,
   },
   {
-    recipient: "VlpcjrQ8mirGC-gsnkYbSscYY-sFkLxkRhjPrbdDfyU",
-    qty: 725694,
-    lockLength: 370435,
-    note: "(#2) Strategic Partner 18-Month Mint Lock",
+    recipient: "NDQwbjnrUIWXyOVyc3k_vn6dzF8ZuqZTy8vQbz5vjSY",
+    qty: 1041667,
+    lockLength: 394200,
   },
   {
-    recipient: "VlpcjrQ8mirGC-gsnkYbSscYY-sFkLxkRhjPrbdDfyU",
-    qty: 725695,
-    lockLength: 501835,
-    note: "(#2) Strategic Partner 24-Month Mint Lock",
+    recipient: "NDQwbjnrUIWXyOVyc3k_vn6dzF8ZuqZTy8vQbz5vjSY",
+    qty: 1041666,
+    lockLength: 525600,
   },
+//
+  {
+    recipient: "3PGmzyVfx42tLV4WmYRImNt1th6NduZmRpXoHVAYGrw",
+    qty: 1041667,
+    lockLength: 262800,
+  },
+  {
+    recipient: "3PGmzyVfx42tLV4WmYRImNt1th6NduZmRpXoHVAYGrw",
+    qty: 1041667,
+    lockLength: 394200,
+  },
+  {
+    recipient: "3PGmzyVfx42tLV4WmYRImNt1th6NduZmRpXoHVAYGrw",
+    qty: 1041666,
+    lockLength: 525600,
+  },
+//
+  {
+    recipient: "t8i_MHAPl-mwDSthhyBAwGWGrI-_foX5SZV_h9f6Vrc",
+    qty: 416667,
+    lockLength: 262800,
+  },
+  {
+    recipient: "t8i_MHAPl-mwDSthhyBAwGWGrI-_foX5SZV_h9f6Vrc",
+    qty: 416667,
+    lockLength: 394200,
+  },
+  {
+    recipient: "t8i_MHAPl-mwDSthhyBAwGWGrI-_foX5SZV_h9f6Vrc",
+    qty: 416666,
+    lockLength: 525600,
+  },
+  //
+  {
+    recipient: "DSQfzhOk75tIqafUIEQ0tF0zzeI-BrfjrrS9CfrFet0",
+    qty: 416667,
+    lockLength: 262800,
+  },
+  {
+    recipient: "DSQfzhOk75tIqafUIEQ0tF0zzeI-BrfjrrS9CfrFet0",
+    qty: 416667,
+    lockLength: 394200,
+  },
+  {
+    recipient: "DSQfzhOk75tIqafUIEQ0tF0zzeI-BrfjrrS9CfrFet0",
+    qty: 416666,
+    lockLength: 525600,
+  },
+    //
+    {
+      recipient: "IU2B15NYim6lgzQiaKK8Dw2gP_t4HcoqKxEQvdqU5yI",
+      qty: 416667,
+      lockLength: 262800,
+    },
+    {
+      recipient: "IU2B15NYim6lgzQiaKK8Dw2gP_t4HcoqKxEQvdqU5yI",
+      qty: 416667,
+      lockLength: 394200,
+    },
+    {
+      recipient: "IU2B15NYim6lgzQiaKK8Dw2gP_t4HcoqKxEQvdqU5yI",
+      qty: 416666,
+      lockLength: 525600,
+    },
+  //
+  {
+    recipient: "to2r4qzI2B5JGGTtFHunyoBbLpG0Ql0YvzlGScvnG5Q",
+    qty: 41667,
+    lockLength: 262800,
+  },
+  {
+    recipient: "to2r4qzI2B5JGGTtFHunyoBbLpG0Ql0YvzlGScvnG5Q",
+    qty: 41667,
+    lockLength: 394200,
+  },
+  {
+    recipient: "to2r4qzI2B5JGGTtFHunyoBbLpG0Ql0YvzlGScvnG5Q",
+    qty: 41666,
+    lockLength: 525600,
+  },
+    //
+    {
+      recipient: "to2r4qzI2B5JGGTtFHunyoBbLpG0Ql0YvzlGScvnG5Q",
+      qty: 41667,
+      lockLength: 262800,
+    },
+    {
+      recipient: "to2r4qzI2B5JGGTtFHunyoBbLpG0Ql0YvzlGScvnG5Q",
+      qty: 41667,
+      lockLength: 394200,
+    },
+    {
+      recipient: "to2r4qzI2B5JGGTtFHunyoBbLpG0Ql0YvzlGScvnG5Q",
+      qty: 41666,
+      lockLength: 525600,
+    }  
 ];
 
 async function mintLocked() {
@@ -56,15 +143,17 @@ async function mintLocked() {
       key,
       "usjm4PCxUd5mtaon7zc97-dt-3qf67yPyqgzLnLqk5A",
       {
-        function: "propose",
-        type: "mintLocked",
-        recipient: vote.recipient,
+        function: "transferLocked",
+        target: vote.recipient,
         qty: vote.qty,
         lockLength: vote.lockLength,
-        note: vote.note,
       }
     );
-    console.log(vote.note, "\n", txID);
+    console.log(JSON.stringify({
+      voteID: txID,
+      recipient: vote.recipient,
+      lockLength: vote.lockLength
+    }));
   }
 }
 
