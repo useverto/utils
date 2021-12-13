75ad1f7d37d10dae2b9e4c41831a93d6e2ca4041
Marton Lederer <marton@lederer.hu>
2021-12-13 19:44:07 +0100

    chore: fmt

--- src/mintLocked.ts
+++ src/mintLocked.ts
@@ -38,7 +38,7 @@ const votes: {
     qty: 1041666,
     lockLength: 525600,
   },
-//
+  //
   {
     recipient: "3PGmzyVfx42tLV4WmYRImNt1th6NduZmRpXoHVAYGrw",
     qty: 1041667,
@@ -54,7 +54,7 @@ const votes: {
     qty: 1041666,
     lockLength: 525600,
   },
-//
+  //
   {
     recipient: "t8i_MHAPl-mwDSthhyBAwGWGrI-_foX5SZV_h9f6Vrc",
     qty: 416667,
@@ -86,22 +86,22 @@ const votes: {
     qty: 416666,
     lockLength: 525600,
   },
-    //
-    {
-      recipient: "IU2B15NYim6lgzQiaKK8Dw2gP_t4HcoqKxEQvdqU5yI",
-      qty: 416667,
-      lockLength: 262800,
-    },
-    {
-      recipient: "IU2B15NYim6lgzQiaKK8Dw2gP_t4HcoqKxEQvdqU5yI",
-      qty: 416667,
-      lockLength: 394200,
-    },
-    {
-      recipient: "IU2B15NYim6lgzQiaKK8Dw2gP_t4HcoqKxEQvdqU5yI",
-      qty: 416666,
-      lockLength: 525600,
-    },
+  //
+  {
+    recipient: "IU2B15NYim6lgzQiaKK8Dw2gP_t4HcoqKxEQvdqU5yI",
+    qty: 416667,
+    lockLength: 262800,
+  },
+  {
+    recipient: "IU2B15NYim6lgzQiaKK8Dw2gP_t4HcoqKxEQvdqU5yI",
+    qty: 416667,
+    lockLength: 394200,
+  },
+  {
+    recipient: "IU2B15NYim6lgzQiaKK8Dw2gP_t4HcoqKxEQvdqU5yI",
+    qty: 416666,
+    lockLength: 525600,
+  },
   //
   {
     recipient: "to2r4qzI2B5JGGTtFHunyoBbLpG0Ql0YvzlGScvnG5Q",
@@ -118,22 +118,22 @@ const votes: {
     qty: 41666,
     lockLength: 525600,
   },
-    //
-    {
-      recipient: "to2r4qzI2B5JGGTtFHunyoBbLpG0Ql0YvzlGScvnG5Q",
-      qty: 41667,
-      lockLength: 262800,
-    },
-    {
-      recipient: "to2r4qzI2B5JGGTtFHunyoBbLpG0Ql0YvzlGScvnG5Q",
-      qty: 41667,
-      lockLength: 394200,
-    },
-    {
-      recipient: "to2r4qzI2B5JGGTtFHunyoBbLpG0Ql0YvzlGScvnG5Q",
-      qty: 41666,
-      lockLength: 525600,
-    }  
+  //
+  {
+    recipient: "to2r4qzI2B5JGGTtFHunyoBbLpG0Ql0YvzlGScvnG5Q",
+    qty: 41667,
+    lockLength: 262800,
+  },
+  {
+    recipient: "to2r4qzI2B5JGGTtFHunyoBbLpG0Ql0YvzlGScvnG5Q",
+    qty: 41667,
+    lockLength: 394200,
+  },
+  {
+    recipient: "to2r4qzI2B5JGGTtFHunyoBbLpG0Ql0YvzlGScvnG5Q",
+    qty: 41666,
+    lockLength: 525600,
+  },
 ];
 
 async function mintLocked() {
@@ -149,11 +149,13 @@ async function mintLocked() {
         lockLength: vote.lockLength,
       }
     );
-    console.log(JSON.stringify({
-      voteID: txID,
-      recipient: vote.recipient,
-      lockLength: vote.lockLength
-    }));
+    console.log(
+      JSON.stringify({
+        voteID: txID,
+        recipient: vote.recipient,
+        lockLength: vote.lockLength,
+      })
+    );
   }
 }
 
